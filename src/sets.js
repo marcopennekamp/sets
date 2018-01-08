a6318f42afc78261ed90c41c1b3d1c6cb7f71191
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-01-08 22:51:58 +0100

    Add exists, union, intersection, difference and symmetricDifference functions.

--- src/sets.js
+++ src/sets.js
@@ -1,6 +1,6 @@
 // @flow
 
-type Collection<A> = Set<A> | Array<A>
+type Collection<A> = Set<A> | Array<A>;
 
 const collectionTypeErrorMessage = 'The argument must be a Set or an Array.';
 
@@ -35,18 +35,77 @@ function toArray<A>(collection: Collection<A>): Array<A> {
 }
 
 /**
- * Applies the function f to each value of the collection, creating a new Set with the results.
+ * Applies the function f to each value of the set, creating a new Set with the results.
  */
 export function map<A, B>(collection: Collection<A>, f: A => B): Set<B> {
   return new Set(toArray(collection).map(f));
 }
 
+/**
+ * Folds the elements of the set with an operation into an accumulator.
+ */
 export function fold<A, X>(collection: Collection<A>, initial: X, f: (X, A) => X): X {
   return toArray(collection).reduce(f, initial);
 }
 
+/**
+ * Returns true if the condition is true for all elements of the set, otherwise false.
+ */
 export function forall<A>(collection: Collection<A>, condition: A => boolean): boolean {
   return toArray(collection).every(condition);
+}
+
+/**
+ * Returns true if the condition is true for one element of the set, otherwise false.
+ */
+export function exists<A>(collection: Collection<A>, condition: A => boolean): boolean {
+  return toArray(collection).some(condition);
+}
+
+/**
+ * Returns the union of the two sets.
+ */
+export function union<A>(c1: Collection<A>, c2: Collection<A>): Set<A> {
+  const set1 = setify(c1);
+  const set2 = setify(c2);
+  const result = new Set(set1);
+  set2.forEach(e => result.add(e));
+  return result;
+}
+
+/**
+ * Returns the intersection of the two sets.
+ */
+export function intersection<A>(c1: Collection<A>, c2: Collection<A>): Set<A> {
+  const set1 = setify(c1);
+  const set2 = setify(c2);
+  const result = new Set();
+  set2.forEach((e) => {
+    if (set1.has(e)) {
+      result.add(e);
+    }
+  });
+  return result;
+}
+
+/**
+ * Returns the the set c1 without the elements from c2.
+ */
+export function difference<A>(c1: Collection<A>, c2: Collection<A>): Set<A> {
+  const set1 = setify(c1);
+  const set2 = setify(c2);
+  const result = new Set(set1);
+  set2.forEach((e) => {
+    result.delete(e);
+  });
+  return result;
+}
+
+/**
+ * Returns the symmetric difference of the two sets.
+ */
+export function symmetricDifference<A>(c1: Collection<A>, c2: Collection<A>): Set<A> {
+  return union(difference(c1, c2), difference(c2, c1));
 }
 
 /**
@@ -61,7 +120,7 @@ export function isSubset<A>(left: Collection<A>, right: Collection<A>): boolean 
 }
 
 /**
- * Checks whether two sets are equal.
+ * Checks whether the two sets are equal.
  */
 export function areEqual<A>(left: Collection<A>, right: Collection<A>): boolean {
   const leftSet = setify(left);
